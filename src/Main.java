5375a60dcff4b46b8a3981df3dfba143513015e6
abwilson5729 <abwilson5729@eagle.fgcu.edu>
2018-09-28 12:28:49 -0400

    //

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Date;
 
 public class Main {
   public static void main (String args[]){
-    Widget w1 = new Widget("name");
+    Widget w1 = new Widget("name");//
     System.out.println(w1.toString());
 
     Widget w2 = new Widget("name2");
